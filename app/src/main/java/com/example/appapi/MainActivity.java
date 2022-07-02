d808df2a5f5e585ef25332408f01b651986a79a8
neutroREM <dhamarloka90@gmail.com>
2022-07-02 06:11:49 -0500

    "LOGIN Y REGISTRO"

--- app/src/main/java/com/example/appapi/MainActivity.java
+++ app/src/main/java/com/example/appapi/MainActivity.java
@@ -4,26 +4,85 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.json.JSONTokener;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
-    public Button btnAdd;
+    Button btnLogin, btnRegistrar;
+    EditText txt_correo, txt_contrasenia;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        btnAdd = findViewById(R.id.btnAdd);
 
+        txt_correo = findViewById(R.id.txt_correo);
+        txt_contrasenia = findViewById(R.id.txt_contrasenia);
+
+        btnLogin = findViewById(R.id.btn_login);
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                alienLogin(txt_correo.getText().toString(), txt_contrasenia.getText().toString());
+            }
+        });
+
+        btnRegistrar = findViewById(R.id.btnRegistrar);
+        btnRegistrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent vistaRegistro = new Intent(MainActivity.this, addUser.class);
+                startActivity(vistaRegistro);
+            }
+        });
 
     }
 
-    public void openAdd(View v){
-        Intent intent = new Intent(this, addUser.class);
-        startActivity(intent);
+    private void alienLogin (final String correo, final String contrasenia){
+        String url = "https://api-alien.herokuapp.com/login";
+        StringRequest loginRequest = new StringRequest(Request.Method.POST, url, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                JSONTokener jsonObject = new JSONTokener(response);
+                String res = response.toString();
+                Toast.makeText(MainActivity.this, "Mensaje = "+ res, Toast.LENGTH_LONG).show();
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.e("error", error.getMessage());
+            }
+        })
+        {
+            protected Map<String, String> getParams(){
+                Map<String, String> params = new HashMap<>();
+                params.put("alien_correo", correo);
+                params.put("alien_contrasenia", contrasenia);
+                return params;
+            }
+        };
+        Volley.newRequestQueue(this).add(loginRequest);
     }
+
+
 
 
 
--- app/src/main/java/com/example/appapi/addUser.java
+++ app/src/main/java/com/example/appapi/addUser.java
@@ -3,12 +3,87 @@ package com.example.appapi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class addUser extends AppCompatActivity {
+
+    EditText txtusuario, txtnombre, txtap, txtam, txtcorreo, txtcontrasenia, txtnac, txtstatus;
+    Button registrar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_user);
+
+        txtusuario = findViewById(R.id.txt_usuario);
+        txtnombre = findViewById(R.id. txt_nombre);
+        txtap = findViewById(R.id.txt_pa);
+        txtam = findViewById(R.id.txt_ma);
+        txtcorreo = findViewById(R.id.txt_correo);
+        txtcontrasenia = findViewById(R.id.txt_contrasenia);
+        txtnac = findViewById(R.id.txt_nac);
+        txtstatus = findViewById(R.id.txt_status);
+
+        registrar = findViewById(R.id.btnRegistrar);
+        registrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                alien_add(txtusuario.getText().toString(), txtnombre.getText().toString(), txtap.getText().toString(), txtam.getText().toString(), txtcorreo.getText().toString(), txtcontrasenia.getText().toString(), txtnac.getText().toString(), txtstatus.getText().toString());
+
+            }
+        });
+    }
+
+    private void alien_add(final String usuario, final String nombre, final String ap, final String am, final String correo, final String contrasenia, final String nac, final String status){
+        String url = "https://api-alien.herokuapp.com/aliens";
+        StringRequest postRequest = new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                try {
+                    JSONObject jsonObject = new JSONObject(response);
+                    Toast.makeText(addUser.this, "Mensaje" + jsonObject.getString("Registro"), Toast.LENGTH_LONG).show();
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.e("Error", error.getMessage());
+            }
+        })
+        {
+            protected Map<String, String> getParams(){
+                Map<String, String> params = new HashMap<>();
+                params.put("alien_usuario", usuario);
+                params.put("alien_nombre", nombre);
+                params.put("alien_ap", ap);
+                params.put("alien_am",am);
+                params.put("alien_correo", correo);
+                params.put("alien_contrasenia", contrasenia);
+                params.put("alien_nac", nac);
+                params.put("alien_status", status);
+                return params;
+            }
+
+        };
+        Volley.newRequestQueue(this).add(postRequest);
+
     }
 }
